f8ad595ec5659fdbb31b80d323d58a3dce5c5690
hfazliddin98 <hatamovfazliddin505@gmail.com>
2023-06-13 23:06:24 +0500

    server

--- pdf/urls.py
+++ pdf/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import pdf, malumot_csv, dekanat_csv
+from .views import pdf, malumot_csv, dekanat_csv, qrcode
 
 urlpatterns = [
     path('', pdf, name='pdf'),
+    path('qrcode/<int:pk>/', qrcode, name='qrcode'),
     path('malumot_csv/', malumot_csv, name='malumot_csv'), 
     path('dekanat_csv/', dekanat_csv, name='dekanat_csv'),
 ]
